3d7ef78cd8b638db4ebe4513c6ecca407c014b4d
Gary <389446599@qq.com>
2018-07-03 16:38:55 +0800

    Initial commit

---
+++ files/目录结构/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o],p,_(q,r,s,t,u,v,w,_(),x,_(y,z,A,B,C,_(D,E,F,G),H,null,I,B,J,B,K,L,M,null,N,O,P,Q,R,S,T,O),U,_(),V,_(),W,_(X,[_(Y,Z,ba,j,bb,bc,s,bd,be,bd,bf,bg,x,_(y,bh,bi,_(bj,bk,bl,bm)),U,_(),bn,_(),bo,_(bp,bq))])),br,_(),bs,_(bt,_(bu,bv)));}; 
+var b="url",c="目录结构.html",d="generationDate",e=new Date(1530606247404.81),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="free",o="vip",p="page",q="packageId",r="fc1f5845a3094d418d8b7f51a3a5b0aa",s="type",t="Axure:Page",u="name",v="目录结构",w="notes",x="style",y="baseStyle",z="627587b6038d43cca051c114ac41ad32",A="pageAlignment",B="near",C="fill",D="fillType",E="solid",F="color",G=0xFFFFFFFF,H="image",I="imageHorizontalAlignment",J="imageVerticalAlignment",K="imageRepeat",L="auto",M="favicon",N="sketchFactor",O="0",P="colorStyle",Q="appliedColor",R="fontName",S="Applied Font",T="borderWidth",U="adaptiveStyles",V="interactionMap",W="diagram",X="objects",Y="id",Z="1f0648f23eb346e09b9afba46e5b8ebd",ba="label",bb="friendlyType",bc="图片 ",bd="imageBox",be="styleType",bf="visible",bg=true,bh="75a91ee5b9d042cfa01b8d565fe289c0",bi="size",bj="width",bk=932,bl="height",bm=837,bn="imageOverrides",bo="images",bp="normal~",bq="images/目录结构/u0.png",br="masters",bs="objectPaths",bt="1f0648f23eb346e09b9afba46e5b8ebd",bu="scriptId",bv="u0";
+return _creator();
+})());
